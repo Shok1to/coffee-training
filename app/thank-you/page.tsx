e7a3b1b9bba80f4a7571ddbc6b21b0be39b1e4b3
Shok1to <shoko.takahashi.911@gmail.com>
2025-05-01 23:21:58 -0400

    fixed thank you page

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -4,7 +4,7 @@ export default function ThankYouPage() {
       <img
         src="/Shokolatte-small.png"
         alt="Shokolatte Logo"
-        className="mb-2 h-40 md:h-56"
+        className="mb-2 h-45 md:h-61"
       />
       <h1 className="mb-4 text-4xl font-bold text-amber-400">Thank You!</h1>
       <p className="mb-8 max-w-md text-gray-300">
